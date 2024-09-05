920812806d3c89aa64808e3e542fb7ecd5f5cadb
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-05 15:18:15 +0700

    3.2. Tạo controllers

--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,14 +1,7 @@
 import { Router, Request, Response } from "express";
-import Topic from "../../models/topic.model";
-
+import * as controller from "../../controllers/client/topic.controller"
 const router:Router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false
-  })
-
-  res.render("client/pages/topics/index");
-});
+router.get("/", controller.topics);
 
 export const topicRoutes: Router = router;
