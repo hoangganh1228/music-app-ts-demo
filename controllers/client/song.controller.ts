47c3a4fdec377ef36ae717230edbdd24a4dfbd52
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-05 21:48:44 +0700

    1.1. Làm trang chi tiết bài hát

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
+
 // [GET] /songs/:slugTopic
 export const list = async (req: Request, res: Response) => {
-    // console.log(req.params.slugTopic);
+    console.log(req.params.slugTopic);
 
     const topic = await Topic.findOne({
         slug: req.params.slugTopic,
@@ -35,5 +36,45 @@ export const list = async (req: Request, res: Response) => {
         pageTitle: topic.title,
         songs: songs
     });
+
+}
+
+// [GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+  const slugSong: String  = req.params.slugSong;
+
+  const song = await Song.findOne({
+    slug: slugSong,
+    deleted: false,
+    status: "active"
+  })
+
+  // console.log(song);
+
+
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    status: "active",
+    deleted: false
+  }).select("fullName")
+
+  // console.log(singer);
+
+
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    deleted: false,
+    status: "active"
+  }).select("title");
+
+  // console.log(topic);
+
+  res.render("client/pages/songs/detail", {
+    pageTitle: song.title,
+    song: song,
+    singer: singer,
+    topic: topic
+
+  });
 
 }
