8d0f519b2d40ccacc51e7e8562c9f4a40f24a451
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-05 16:19:04 +0700

    4. Làm trang Chủ đề bài hát (bên client)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -4,14 +4,16 @@ import Topic from "../../models/topic.model";
 // [GET] /topics/
 export const topics = async (req: Request, res: Response) => {
   const topics = await Topic.find({
-    deleted: false
+    deleted: false,
+    status: "active"
   });
 
 
   
 
   res.render("client/pages/topics/index", {
-    pageTitle: "Chủ đề bài hát"
+    pageTitle: "Chủ đề bài hát",
+    topics: topics
   });
   
 }
