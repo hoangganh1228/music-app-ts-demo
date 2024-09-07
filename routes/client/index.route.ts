9be7ee7a8b3252142335c249de1b4f392fe762f6
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-07 16:47:25 +0700

    2.3. Làm trang Bài hát yêu thích

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { topicRoutes } from "./topic.route";
 import { songRoutes } from "./song.route";
 import { searchRoutes } from "./search.route";
+import { favoriteSongRoutes } from "./favorite-song.route";
 
 
 const clientRoutes = (app: Express): void => {
@@ -10,6 +11,9 @@ const clientRoutes = (app: Express): void => {
   app.use("/songs", songRoutes);
 
   app.use(`/search`, searchRoutes);
+
+  app.use(`/favorite-songs`, favoriteSongRoutes);
+
 }
 
 export default clientRoutes
