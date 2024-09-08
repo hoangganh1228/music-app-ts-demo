0d5056974700c875951fe64d8743c89d6296dbc9
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-09-08 13:00:33 +0700

    4. Tạo trang Danh sách bài hát (bên admin)

--- routes/admin/topic.route.ts
+++ routes/admin/topic.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router: Router = Router();
 
-import * as controller from "../../controllers/admin/topic.controller";
+import * as controller from "../../controllers/admin/../../controllers/admin/topic.controller";
 
 router.get("/", controller.index);
 
